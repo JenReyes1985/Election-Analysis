b36a3071d82cd3dc62125838cc5a9d36e2e88e4e
JenReyes1985 <95717772+JenReyes1985@users.noreply.github.com>
2022-01-16 15:25:09 -0600

    Add PyPoll.py File.

---
+++ PyPoll.py
@@ -0,0 +1,34 @@
+#The data we need to retrieve.
+#1 The total number of votes case
+#2 A complete list of candidates who received votes
+#3 The percentage of votes each candidate won
+#4 The total number of votes each candidate won
+#5 The winner of the election based on popular vote
+
+#Add our dependencies
+import csv
+import os
+from wsgiref.headers import Headers
+#Assign a variable for the file to load and the path.
+file_to_load= os.path.join("Resources","election_results.csv")
+
+#Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis","election_analysis.txt")
+
+#Open the election results and read the file.
+with open(file_to_load) as election_data:
+    file_reader=csv.reader(election_data)
+
+    #Read and Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+#Using the with statement open the file as a text file.
+with open(file_to_save,"w") as txt_file:
+
+    #Write some data to the file.
+    txt_file.write("Counties in the Election\n\nArapahoe\nDenver\nJefferson")
+    
+
+#Close the file.
+election_data.close()
